04c648b45ca78f01496b1a94c2311930c283fdf1
alexsonphoenix <alexsonphoenix>
2019-12-20 17:48:27 +1300

    setup the models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Topping, Pizza_type, Pizza, Sub_addon, Sub, Pasta, Salad, Dinner_platter
 
 # Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Pizza_type)
+admin.site.register(Topping)
+admin.site.register(Sub)
+admin.site.register(Sub_addon)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Dinner_platter)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+class Topping(models.Model):
+    """docstring for Topping."""
+    topping_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.topping_name}"
+
+
+class Pizza_type(models.Model):
+    """docstring for Pizza_type."""
+    Pizza_type_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.Pizza_type_name}"
+
+
+class Pizza(models.Model):
+    """docstring for Pizza."""
+    pizza_type = models.ForeignKey(Pizza_type, on_delete=models.CASCADE, related_name="pizza_kind")
+    pizza_size = models.CharField(max_length=10)
+    topping_type = models.CharField(max_length=20)
+    toppings = models.ManyToManyField(Topping, verbose_name="list of toppings")
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.pizza_type} - {self.pizza_size} - {self.topping_type} - ({self.price})"
+
+class Sub_addon(models.Model):
+    """docstring for Sub_addon."""
+    sub_addon_name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.topping_name} - ({self.price})"
+
+
+class Sub(models.Model):
+    """docstring for Subs."""
+    sub_name = models.CharField(max_length=64)
+    sub_size = models.CharField(max_length=10)
+    sub_addon = models.ManyToManyField(Sub_addon, verbose_name="list of sub_addons")
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.sub_name} - {self.sub_size} (With {self.sub_addon}) - ({self.price})"
+
+
+class Pasta(models.Model):
+    """docstring for Pasta."""
+    pasta_name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.pasta_name} - ({self.price})"
+
+
+class Salad(models.Model):
+    """docstring for Salad."""
+    salad_name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.salad_name} - ({self.price})"
+
+
+
+class Dinner_platter(models.Model):
+    """docstring for Salad."""
+    plate_name = models.CharField(max_length=64)
+    plate_size = models.CharField(max_length=10)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.plate_name} - {self.plate_size} - ({self.price})"
